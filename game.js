4c3da799bf012e763bc93c7dab6b4dc34141fe67
amine <62792200+amive@users.noreply.github.com>
2025-03-09 12:11:43 +0100

    Update game.js

--- game.js
+++ game.js
@@ -308,7 +308,7 @@ SFX.start.src = "sfx/start.wav";
 SFX.flap.src = "sfx/flap.wav";
 SFX.score.src = "sfx/score.wav";
 SFX.hit.src = "sfx/hit.wav";
-SFX.die.src = "sfx/Bilal.wav";
+SFX.die.src = "sfx/Bilal2.wav";
 
 function gameLoop() {
   update();
